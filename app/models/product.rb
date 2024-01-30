c0aa2ace792feb113c0251ce355e05291701ef06
mcr45 <depr3604@protonmail.com>
2024-01-29 20:25:12 -0600

    boh

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :name, presence:true,uniqueness:true,length:{minimum:0,maximum:50}
+    validates :price,presence:true,numericality:{greater_than:0}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_010917) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_020707) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "categories_id"
+    t.index ["categories_id"], name: "index_products_on_categories_id"
   end
 
+  add_foreign_key "products", "categories", column: "categories_id"
 end
